51cc4fe79d8dd323bdcee1b890bcbec14de7eb4d
Ekaterina Latushkina <katia_sali@mail.ru>
2020-10-30 11:47:24 +0300

    feat: add directives for list courses

--- tasks/src/app/app.module.ts
+++ tasks/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { HomeComponent } from './components/home/home.component';
 import { TimePipe } from './pipes/list-time.pipe';
 import { DateDirective } from './directives/list-date.directive'
 import { ListFilterPipe } from './pipes/list-filter.pipe';
-import { ListDateBorderPipe } from './pipes/list-date-border.pipe';
 
 
 @NgModule({
@@ -34,7 +33,6 @@ import { ListDateBorderPipe } from './pipes/list-date-border.pipe';
     TimePipe,
     DateDirective,
     ListFilterPipe,
-    ListDateBorderPipe
   ],
   imports: [
     BrowserModule,
--- tasks/src/app/directives/list-date.directive.ts
+++ tasks/src/app/directives/list-date.directive.ts
@@ -1,14 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+import * as moment from 'moment';
 
 @Directive({
     selector: '[dateOfCreate]'
 })
 
-export class DateDirective {
-    public today: number = Date.now();
-    
-    // .toLocaleDateString()
-    constructor(el: ElementRef) {
-        console.log(this.today)
+export class DateDirective implements AfterViewInit {
+
+    @Input() itemDate: string;
+    constructor(public el: ElementRef) {
+    }
+
+    public ngAfterViewInit(): void {
+        const itemValue = this.itemDate;
+        const howManyDays = moment(itemValue).fromNow();
+        const numDays = +howManyDays.split(' ')[0];
+        if (numDays < 14 && numDays > 0) {
+            this.el.nativeElement.style.border = '3px solid green'
+        } else {
+            this.el.nativeElement.style.border = '3px solid blue'
+        }
     }
 }
